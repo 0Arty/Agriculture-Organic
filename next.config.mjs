072b07ff6f79d3163924cce91a6022a0596ef500
0Arty <gcf3223344@gmail.com>
2024-03-22 16:00:29 +0200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-export default nextConfig;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const stylesPath = path.join(__dirname, 'src', 'styles')
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [stylesPath],
+    prependData: `@use 'variables' as *;
+    @use 'mixins' as *;`,
+  },
+}
+
+export default nextConfig
